7b7cc963df1f1c97d67351e600f03e373f749de7
Faustino Ruiz <ftr1989@gmail.com>
2024-07-05 00:02:08 -0700

    4

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -3,24 +3,19 @@ import React, { useState } from 'react'
 import ColorBlock from './ColorBlock'
 
 function App(){
-    let [colors, setColors] = useState([
+    let [colors] = useState([
         'violet', 'blue',
         'lightblue', 'green',
         'greenyellow', 'yellow',
         'orange', 'red'
     ])
     
-    let colorMap = colors.map((color, i) => {
-        return (
-            <ColorBlock color={color} />
-        )
-    })
 
     return (
       <div className="App">
-    {colors.map((color, i) => 
+    {colors.map((color, i) => (
         <ColorBlock key={i} color={color} />
-    )}
+    ))}
 </div>
 
     )
